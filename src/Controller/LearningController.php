f20fcd5ecfa4929e36f5afaaf4ca4c42ccd4fc6a
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-11-06 11:45:38 +0100

    finished mvc step 2

---
+++ src/Controller/LearningController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LearningController extends AbstractController
+{
+    /**
+     * @Route("/learning", name="learning")
+     */
+    public function index(): Response
+    {
+        return $this->render('learning/index.html.twig', [
+            'controller_name' => 'LearningController',
+        ]);
+    }
+
+    public function aboutMe(){
+        {
+            return $this->render('learning/aboutme.html.twig', ['name' => 'BeCode']);
+        }
+    }
+
+}
